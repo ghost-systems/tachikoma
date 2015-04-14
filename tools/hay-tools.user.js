9c29ab6441bd42d4930abf1d792825e8ba858f73
Ghost Systems Admin <admin@ghost.systems>
2015-04-14 12:19:03 +0100

    Change website tools activate on

--- tools/hay-tools.user.js
+++ tools/hay-tools.user.js
@@ -3,9 +3,9 @@
 // @description Get product data from hamiltonandyoungtrade.co.uk
 // @namespace   lab.ghost.systems
 // @noframes
-// @include     http://hamiltonandyoungtrade.co.uk/*
+// @include     http://www.hamiltonandyoungtrade.co.uk/*
 // @grant		none
-// @version     7.0.0
+// @version     7.0.2
 // ==/UserScript==
 
 (function($) {
@@ -19,7 +19,7 @@
 			$('body').html('<pre>' + self.allText + '</pre>');
 		}
 
-		this.label = 'Hamilton & Young Toolset';	
+		this.label = 'H & Y Toolset';	
 		this.tools = 
 		[
 			['Show Saved Info', self.showSavedInfo],
